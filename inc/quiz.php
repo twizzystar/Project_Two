ad03dc83d8d2051fdb7c234e0cc002166b42300c
Twynette Ashby <tashby1@gmail.com>
2019-11-25 21:16:48 -0500

    functions.php
    changed if statements

--- inc/quiz.php
+++ inc/quiz.php
@@ -17,6 +17,7 @@
 
 // Include questions
 include "questions.php";
+include "functions.php";
 // Keep track of which questions have been asked
 $question = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_NUMBER_INT);
 if (empty ($question)){
@@ -24,36 +25,43 @@ if (empty ($question)){
   $score = 0;
   $question = 1;
 }
-if(isset($_POST['selection'])){
-   $_SESSION['selection'][$question-1] = filter_input(Input_POST, 'selection', FILTER_SANITIZE_STRING);
+if(isset($_POST['answer'])){
+   $_SESSION['answer'][$question-1] = filter_input(Input_POST, 'answer', FILTER_SANITIZE_STRING);
 }
-//if (isset($_POST['correct'])) {
-        //$_SESSION['correct'] = filter_input(INPUT_POST, 'correct', FILTER_SANITIZE_NUMBER_INT);
-// Show which question they are on
+/*if (isset($_POST['answer'])) {
+   $_SESSION['answer'] = filter_input(INPUT_POST, 'answer', FILTER_SANITIZE_STRING);
+}*/
+if (isset($_POST['correct'])) {
+        $_Session['correct'] = filter_input(INPUT_POST, 'correct',
+FILTER_SANITIZE_NUMBER_INT);
+}
+//Show which question they are on
 // Show random question
 // Shuffle answer buttons
-shuffle($questions);
+shuffle($_SESSION['questions']);
 $choices = ['correctAnswer', 'firstIncorrectAnswer', 'secondIncorrectAnswer'];
 shuffle($choices);
 echo "<p class='breadcrumbs'>Question " . $question . " of 10</p>";
-    echo "<p class='quiz'>What is " . $questions[$question]["leftAdder"] . " + " . $questions[$question]["rightAdder"] . "?</p>";
-    var_dump($questions[$question]["leftAdder"]);
+    echo "<p class='quiz'>What is " . $questions[$question-1]["leftAdder"] . " + " . $questions[$question-1]["rightAdder"] . "?</p>";
     echo "<form action='index.php?p=" . ($question+1) . "' method='post'>";
 
-    echo "<input type='submit' class='btn' name='answer' value='" . $questions[$question][$choices[0]] . "'>";
-    echo "<input type='submit' class='btn' name='answer' value='" . $questions[$question][$choices[1]] . "'>";
-    echo "<input type='submit' class='btn' name='answer' value='" . $questions[$question][$choices[2]] . "'>";
-    echo "<input type='hidden' name='correct' value='" . $questions[$question]['correctAnswer'] . "'>";
-    echo "<p class='quiz'>What is " . $questions[$question]["leftAdder"] . " + " . $questions[$question]["rightAdder"] . "?</p>";
-  // var_dump($questions[$question]["leftAdder"]);
-  // var_dump($questions[$question]["leftAdder"]);
-   echo "<form action='index.php?p=" . ($question+1) . "' method='post'>";
-   //echo "<input type='submit' class='btn' name='answer' value='" . $questions[$question][$choices[0]] . "'>";
+    echo "<input type='submit' class='btn' name='answer' value='" . $questions[$question-1][$choices[0]] . "'>";
+    echo "<input type='submit' class='btn' name='answer' value='" . $questions[$question-1][$choices[1]] . "'>";
+    echo "<input type='submit' class='btn' name='answer' value='" . $questions[$question-1][$choices[2]] . "'>";
+    echo "<input type='hidden' name='correct' value='" . $correctAnswer . "'>";
 
 // Toast correct and incorrect answers
+    toasts ();
 // Keep track of answers
 // If all questions have been asked, give option to show score
 // else give option to move to next question
-
+if ($question_num == 11) {
+    echo '<h1> You got '. $_SESSION['numberCorrect'] . ' out of 10 correct!</h1>';
+    echo "<form action='index.php' method='GET'>";
+    echo "<input type='submit' class='btn' name='restart' value='Try Again?'>";
+    session_destroy();
+    echo '</form>';
+}
 
 // Show score
+// Show score & button to restart
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
   session_start();
+//if(isset($_SESSION['answer']))
+//{
+//header('Location:functions.php);
+//exit;
+//}
 ?>
 <!DOCTYPE html>
 <html lang="en">
